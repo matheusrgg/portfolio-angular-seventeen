31eac0a2c901a9628d63848eff36a303c73c5ca1
matheusrgg <matheusrgg@gmail.com>
2024-06-27 16:00:42 -0300

    footer config anda translation finalization

--- src/app/components/organs/projects/projects.component.ts
+++ src/app/components/organs/projects/projects.component.ts
@@ -4,67 +4,62 @@ import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
-  projects: any = [
-
-
-    {
-      id: 0,
-      heading: 'IBM',
-      Data: '01/11/2022 - Até o momento',
-      img: '../assets/ibm-website.png',
-      description: 'Meu primeiro freela, tive a oportunidade de fazer um site para a empresa de estacionamento do meu tio, usando html, javascript e bootstrap! Foi muito bacana pq eu criei todo o design do site. Hoje olhando vejo q mudaria bastante coisa, más foi um ótimo começo.',
-      tecnologia: 'HTML e Bootstrap',
-      link: "https://www.ibm.com/"
-    },
-    {
-      id: 1,
-      heading: 'Metodo Consultoria',
-      Data: '05/03/2021 - 10/05/2021',
-      img: '../assets/metodoconsultoria.png',
-      description: 'Foi uma plataforma web que fiz em Php e Wordpress para uma empresa de Treinamento ele tb foi totalmente customiza, elaboramos logo, icone , imagens e paleta de cores, totalmente novos. ALém disso com alguns plugins do wordpress fizemos algumas atividade para treinamento dinämico, ficou bem legal!',
-      tecnologia: 'Wordpress, MySQL e Php',
-      link: "https://metodoconsultoria.com/"
-    },
-    {
-      id: 2,
-      heading: 'Ok Park',
-      Data: '05/12/2019',
-      img: '../assets/okpark.png',
-      description: 'Meu primeiro freela, tive a oportunidade de fazer um site para a empresa de estacionamento do meu tio, usando html, javascript e bootstrap! Foi muito bacana pq eu criei todo o design do site. Hoje olhando vejo q mudaria bastante coisa, más foi um ótimo começo.',
-      tecnologia: 'HTML e Bootstrap',
-      link: "http://www.okparkestacionamentos.com/"
-    },
-
-    {
-      id: 3,
-      heading: 'Enertu',
-      Data: '17/08/2023 - Até o momento',
-      img: '../assets/enerty.png',
-      description: 'Um projeto que comecei a trabalhar recentemente sobre gestão compartilhada de energia usando placa solar , tipo um buscapé com anúncios, bem interesssante.',
-      tecnologia: 'React, Next e Node',
-      link: ""
-    },
-
-
-
-  ]
+  projects: any = [];
 
   constructor(
     private translate: TranslateService,
   ) {
-    this.translate.setDefaultLang("pt")
+    this.translate.setDefaultLang('pt');
+    this.loadProjects();
   }
+
   switchLanguage(language: string) {
-    console.log("click")
-    this.translate.use(language)
-
+    console.log('click');
+    this.translate.use(language);
+    this.loadProjects();
   }
 
-  teste(c: any) {
-    return console.log("cade", c);
+  loadProjects() {
+    this.projects = [
+      {
+        id: 0,
+        heading: this.translate.instant('PROJECTS.HEADING_0'),
+        data: this.translate.instant('PROJECTS.DATA_0'),
+        img: '../assets/ibm-website.png',
+        description: this.translate.instant('PROJECTS.DESCRIPTION_0'),
+        tecnologia: this.translate.instant('PROJECTS.TECNOLOGIA_0'),
+        link: 'https://www.ibm.com/'
+      },
+      {
+        id: 1,
+        heading: this.translate.instant('PROJECTS.HEADING_1'),
+        data: this.translate.instant('PROJECTS.DATA_1'),
+        img: '../assets/metodoconsultoria.png',
+        description: this.translate.instant('PROJECTS.DESCRIPTION_1'),
+        tecnologia: this.translate.instant('PROJECTS.TECNOLOGIA_1'),
+        link: 'https://metodoconsultoria.com/'
+      },
+      {
+        id: 2,
+        heading: this.translate.instant('PROJECTS.HEADING_2'),
+        data: this.translate.instant('PROJECTS.DATA_2'),
+        img: '../assets/okpark.png',
+        description: this.translate.instant('PROJECTS.DESCRIPTION_2'),
+        tecnologia: this.translate.instant('PROJECTS.TECNOLOGIA_2'),
+        link: 'http://www.okparkestacionamentos.com/'
+      },
+      {
+        id: 3,
+        heading: this.translate.instant('PROJECTS.HEADING_3'),
+        data: this.translate.instant('PROJECTS.DATA_3'),
+        img: '../assets/enerty.png',
+        description: this.translate.instant('PROJECTS.DESCRIPTION_3'),
+        tecnologia: this.translate.instant('PROJECTS.TECNOLOGIA_3'),
+        link: ''
+      },
+    ];
   }
 }
-
